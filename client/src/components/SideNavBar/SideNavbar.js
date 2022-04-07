137eb293b2e9f0c05060d7f21a4662dcb9eccc92
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-04-06 23:24:06 -0400

    Basic Sidebar

--- client/src/components/SideNavBar/SideNavbar.js
+++ client/src/components/SideNavBar/SideNavbar.js
@@ -5,7 +5,29 @@ function SideNavBar() {
 
     return (
       <React.Fragment>
-          <h1 className='nuts'>Big Nuts</h1>
+        <div className='full-nav'>
+            <img className='logo' src={require("./TryColorsLogo.png")} />
+            <div className='nav-space'>
+                <span className='nav-title'>WORKSPACE</span>
+            </div>
+            <div>
+                <a className="nav-link" href="/">Mixer</a>
+                <a className="nav-link" href="/Game">Game</a>
+            </div>
+            <div className='nav-space'>
+                <span className='nav-title'>ACCOUNT</span>
+            </div>
+            <div>
+                <a className="nav-link" href="/SignIn">Sign In</a>
+            </div>
+            <div className='nav-space'>
+                <span className='nav-title'>GALLERY</span>
+            </div>
+            <div>
+                <a className="nav-link" href="/Colors">All Colors</a>
+                <a className="nav-link" href="/Palettes">All Palettes</a>
+            </div>
+        </div>
       </React.Fragment>
     )
   }
